ed6ada6f5705da9b402e8538e2a362b9c0b3d6de
Ivan <ivan.inclan.327b@gmail.com>
2022-12-23 19:17:58 -0600

    Se agrego el logo

--- src/ui/components/Navbar.tsx
+++ src/ui/components/Navbar.tsx
@@ -1,15 +1,17 @@
 import { Link, NavLink } from 'react-router-dom';
+import kalyptio from '../../assets/img/kalyptio.png'
 
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
+        
             <Link 
             className="navbar-brand" 
             to="/"
             >
-                Parkings
+                <img src={kalyptio} className="logo img-fluid"/>
             </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
